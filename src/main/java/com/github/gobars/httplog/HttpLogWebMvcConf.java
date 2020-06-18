58374d7b567e1e0d37bff246ba89bd644bb95996
bingoo <bingoo.huang@gmail.com>
2020-06-18 23:59:30 +0800

    addArgumentResolvers

--- src/main/java/com/github/gobars/httplog/HttpLogWebMvcConf.java
+++ src/main/java/com/github/gobars/httplog/HttpLogWebMvcConf.java
@@ -3,8 +3,11 @@ package com.github.gobars.httplog;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -18,5 +21,10 @@ public class HttpLogWebMvcConf extends WebMvcConfigurationSupport {
     registry.addInterceptor(httpLogInterceptor).addPathPatterns("/**");
     log.info("Configure Interceptor.....");
     super.addInterceptors(registry);
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(new HttpLogCustomArgumentResolver());
   }
 }
