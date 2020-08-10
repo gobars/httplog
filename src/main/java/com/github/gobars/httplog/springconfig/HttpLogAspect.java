2cc403a4a3be1fc94e65377a2260c51953d2cb8a
bingoohuang <bingoo.huang@gmail.com>
2020-08-10 08:52:34 +0800

    refactor

--- src/main/java/com/github/gobars/httplog/springconfig/HttpLogAspect.java
+++ src/main/java/com/github/gobars/httplog/springconfig/HttpLogAspect.java
@@ -1,11 +1,12 @@
 package com.github.gobars.httplog.springconfig;
 
+import static org.springframework.core.annotation.AnnotationUtils.getAnnotationAttributes;
+
 import com.github.gobars.httplog.HttpLog;
 import com.github.gobars.httplog.HttpLogAttr;
 import com.github.gobars.httplog.HttpLogCustom;
 import com.github.gobars.httplog.HttpLogFork;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -14,7 +15,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.AnnotationAttributes;
-import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -27,60 +27,64 @@ public class HttpLogAspect {
    * @param jp 拦截点
    * @param httpLog 注解
    */
-  @Around("@annotation(httpLog)") // the pointcut expression
+  @Around("@annotation(httpLog)")
   public Object pointcut(ProceedingJoinPoint jp, HttpLog httpLog) throws Throwable {
-    log.debug("pointcut httpLog got:{}", httpLog);
-
-    if (jp.getTarget().getClass().getSimpleName().contains("Controller")) {
-      log.debug("pointcut httpLog ignored:{} Controller", httpLog);
+    if (shouldBypass(jp, httpLog)) {
       return jp.proceed();
     }
 
-    String prefix = "org.springframework.web.bind.annotation";
-    if (anyAnnStarts(jp.getTarget().getClass().getAnnotations(), prefix)) {
-      log.debug("pointcut httpLog ignored:{} Class", httpLog);
-
-      return jp.proceed();
-    }
-
-    Method method = ((MethodSignature) jp.getSignature()).getMethod();
-    if (anyAnnStarts(method.getAnnotations(), prefix)) {
-      log.debug("pointcut httpLog ignored:{} Method", httpLog);
-
-      return jp.proceed();
-    }
-
-    log.debug("pointcut httpLog ok:{}", httpLog);
-
-    val attr =
-        HttpLogAttr.create(
-            new AnnotationAttributes(AnnotationUtils.getAnnotationAttributes(httpLog)));
+    val attr = HttpLogAttr.create(new AnnotationAttributes(getAnnotationAttributes(httpLog)));
     Object req = null;
     if (jp.getArgs().length > 0) {
       req = jp.getArgs()[0];
     }
 
     HttpLogFork f = HttpLogCustom.fork(attr, req);
-    f.setMethod(method.getName());
+    f.setMethod(((MethodSignature) jp.getSignature()).getMethod().getName());
+
     HashMap<String, String> last = HttpLogCustom.get().getMap();
     HashMap<String, String> thisMap = new HashMap<>();
     HttpLogCustom.get().setMap(thisMap);
 
     try {
       Object result = jp.proceed();
-
-      f.customAll(thisMap);
-      HttpLogCustom.get().setMap(last);
-
       f.submit(result);
       return result;
     } catch (Throwable t) {
-      f.customAll(thisMap);
-      HttpLogCustom.get().setMap(last);
-
       f.submitError(t);
       throw t;
+    } finally {
+      f.customAll(thisMap);
+      HttpLogCustom.get().setMap(last);
     }
+  }
+
+  private boolean shouldBypass(ProceedingJoinPoint jp, HttpLog httpLog) {
+    log.debug("pointcut httpLog got:{}", httpLog);
+
+    Class<?> targetClass = jp.getTarget().getClass();
+    if (targetClass.getSimpleName().contains("Controller")) {
+      log.debug("pointcut httpLog ignored:{} Controller", httpLog);
+      return true;
+    }
+
+    // since all @Controller @RestController  @RequestMapping @XyzMapping are all in
+    // this package, we will bypass it to skip @HttpLog in Controller's methods.
+    String prefix = "org.springframework.web.bind.annotation";
+    if (anyAnnStarts(targetClass.getAnnotations(), prefix)) {
+      log.debug("pointcut httpLog ignored:{} Class", httpLog);
+
+      return true;
+    }
+
+    if (anyAnnStarts(((MethodSignature) jp.getSignature()).getMethod().getAnnotations(), prefix)) {
+      log.debug("pointcut httpLog ignored:{} Method", httpLog);
+
+      return true;
+    }
+
+    log.debug("pointcut httpLog ok:{}", httpLog);
+    return false;
   }
 
   public static boolean anyAnnStarts(Annotation[] annotations, String webBind) {
